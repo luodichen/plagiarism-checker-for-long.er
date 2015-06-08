b6cdb037e1611c5b2f85e3b0c77ffd99423d08cf
luodichen <me@luodichen.com>
2015-06-08 17:10:19 +0800

    update client

--- src/client/client.py
+++ src/client/client.py
@@ -8,11 +8,14 @@ import urllib2
 import json
 import time
 import analysis
+import sys
 from urllib import urlencode
 
-API_URL = "http://localhost:8000/api"
+global API_URL
+API_URL = None
 
 def quest_task():
+    global API_URL
     ret = None
     url = API_URL + "/questtask/"
     
@@ -27,6 +30,7 @@ def quest_task():
     return ret
 
 def report_progress(task_id, progress):
+    global API_URL
     url = API_URL + "/report/progress/"
     data = {'id' : task_id, 'progress' : progress}
     
@@ -36,6 +40,7 @@ def report_progress(task_id, progress):
         pass
     
 def report_result(task_id, result, resultmeta):
+    global API_URL
     url = API_URL + "/report/result/"
     data = {'id' : task_id, 'result' : result, 'resultmeta' : resultmeta}
     
@@ -48,6 +53,7 @@ def report_result(task_id, result, resultmeta):
     return response['error']
 
 def report_meta(task_id, metadata):
+    global API_URL
     url = API_URL + "/report/meta/"
     data = {'id' : task_id, 'title' : metadata['title'], 
             'author' : metadata['submit'], 'metadata' : json.dumps(metadata)}
@@ -60,13 +66,18 @@ def report_meta(task_id, metadata):
     
     return response['error']
 
-def main():
+def main(argv):
+    global API_URL
+    API_URL = argv[1]
+    
+    print "Server: " + API_URL
+        
     while True:
         try:
             task =  quest_task()
             if None == task:
-                print "no more task now, waiting for 10 seconds......"
-                time.sleep(10)
+                print "no more task now, waiting for 20 seconds......"
+                time.sleep(20)
                 continue
             
             url = task['url']
@@ -96,4 +107,5 @@ def main():
             time.sleep(30)
             continue
     
-main()
+if __name__ == '__main__':
+    main(sys.argv)
